7d49914bb9c690a8843d9e0ed303268774df71d1
Estefanía <yuukoleviathan@gmail.com>
2026-09-19 18:51:34 -0500

    Agregar clase Usuario

---
+++ src/main/java/com/biblioteca/modelo/Usuario.java
@@ -0,0 +1,32 @@
+package com.biblioteca.modelo;
+
+// Representa al usuario que solicita prestamos en la biblioteca
+public class Usuario {
+
+    private String id;
+    private String nombre;
+    private String correo;
+
+    public Usuario(String id, String nombre, String correo) {
+        this.id = id;
+        this.nombre = nombre;
+        this.correo = correo;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " (" + id + ")";
+    }
+}
